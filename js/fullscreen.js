de3eeaeb4c354ab2c90e861b28957adf80608af8
Dimitry Patt <dimitry.patt@hpe.com>
2016-06-15 18:40:41 +0300

    added full screen toggle

---
+++ js/fullscreen.js
@@ -0,0 +1,16 @@
+
+function goFull(){
+	launchIntoFullscreen(document.getElementById("cvs"));
+}
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+
